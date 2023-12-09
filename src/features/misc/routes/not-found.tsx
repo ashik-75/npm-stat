c02b59f5eddfa99ad2f881115b4bf3bf1dbdb525
ashik-75 <ashikex49@gmail.com>
2023-12-09 22:55:23 +0600

    update not found page

--- src/features/misc/routes/not-found.tsx
+++ src/features/misc/routes/not-found.tsx
@@ -1,14 +1,30 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NotFound: React.FC = () => {
-  const location = useLocation();
   return (
-    <div className="flex flex-col items-center gap-3 rounded-xl bg-rose-600 text-white">
-      <div className="text-5xl font-extrabold">404</div>
-      <h1 className="text-xl">Page Not Found</h1>
-      <p>Recheck this URL: {location.pathname}</p>
-    </div>
+    <main className="sm:py-32 lg:px-8 grid min-h-full place-items-center bg-white px-6 py-24">
+      <div className="text-center">
+        <p className="text-base font-semibold text-indigo-600">404</p>
+        <h1 className="sm:text-5xl mt-4 text-3xl font-bold tracking-tight text-gray-900">
+          Page not found
+        </h1>
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            to="/"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Go back home
+          </Link>
+          <Link to="/contact" className="text-sm font-semibold text-gray-900">
+            Contact support <span aria-hidden="true">&rarr;</span>
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 };
 
