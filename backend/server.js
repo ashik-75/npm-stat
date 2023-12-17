6d9ddf5adc09ecb5086192b137d731de7645489a
ashik-75 <ashikex49@gmail.com>
2023-12-17 12:49:57 +0600

    backend update

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,29 @@
 import express from "express";
 
 const API_URL = "https://registry.npmjs.org";
+const RANGE_URL = "https://api.npmjs.org";
 const app = express();
 
 app.get("/:package", async (req, res) => {
   try {
     const pkg = req.params.package;
     const response = await fetch(`${API_URL}/${pkg}`).then((dt) => dt.json());
+
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong!",
+    });
+  }
+});
+
+app.get("/downloads/:range/:pkg", async (req, res) => {
+  try {
+    const pkg = req.params.pkg;
+    const range = req.params.range;
+    const response = await fetch(
+      `${RANGE_URL}/downloads/range/${range}/${pkg}`
+    ).then((dt) => dt.json());
 
     res.json(response);
   } catch (error) {
